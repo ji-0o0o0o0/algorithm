7a7e3fcd6287c76fbb1c69788167b70e6da1ed65
ji-0o0o0o0 <tlswldud328@gmail.com>
2025-01-11 14:22:31 +0900

    Add files via upload

---
+++ JAVA/src/j08_유효한팰린드롬.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class j08_유효한팰린드롬 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] list = sc.nextLine().split(" ");
+    }
+}
