1a1d3da850908ebfb5774ce7cf9994066b7e1c25
ji-0o0o0o0 <tlswldud328@gmail.com>
2025-01-14 23:34:00 +0900

    Create bj_1178.java

---
+++ JAVA/backjoon/bj_1178.java
@@ -0,0 +1,14 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class bj_1178 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String s;
+        while((s=bufferedReader.readLine())!=null){
+            System.out.println(s);
+        }
+    }
+}
