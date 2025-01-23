0d5f222757cb81c3463d5627c660ccf87bd2ca74
ji-0o0o0o0 <tlswldud328@gmail.com>
2025-01-23 23:32:27 +0900

    Create bj_119545.java

---
+++ JAVA/backjoon/bj_119545.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class bj_119545 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+
+        for (int i = 0; i <x ; i++) {
+            while (sc.hasNext()){
+                StringBuilder sb = new StringBuilder();
+                System.out.println(sb.reverse());
+            }
+        }
+        sc.close();
+    }
+}
